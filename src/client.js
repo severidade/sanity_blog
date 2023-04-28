853c8821fe7a5eb1f5897b4ced2fc0957586984a
severo <oliveira.severo@gmail.com>
2023-04-28 20:50:07 -0300

    docs: :memo:

--- src/client.js
+++ src/client.js
@@ -5,6 +5,6 @@ export default sanityClient({
   dataset: 'production',
   // as informações acima estão no arquivo sanity.cli.js
   useCdn: true, // set to `true` to fetch from edge cache
-  apiVersion: '2023-04-27', // use current date (YYYY-MM-DD) to target the latest API version
+  apiVersion: '2023-04-28', // use current date (YYYY-MM-DD) to target the latest API version
   // token: process.env.SANITY_SECRET_TOKEN // Only if you want to update content with the client
 })
