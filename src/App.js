753b00d4b31e1e8020646d30c49b84ff8922e3dc
severo <oliveira.severo@gmail.com>
2023-04-28 23:01:40 -0300

    feat: :rocket: Cria as rotas

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
-function App() {
+import './Reset.css';
+import './App.css';
+import Navbar from './componentes/Navbar';
+import Home from './pages/Home';
+import SinglePost from './pages/SinglePost';
+import BlogPosts from './pages/BlogPosts';
+import Error from './pages/Error';
+
+export default function App(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Switch>
+        <Route  component={Home} exact path="/"/>
+        <Route  component={SinglePost} path="/post/:slug" />
+        <Route  component={BlogPosts} path="/post/" />
+        <Route  component={Error} path="*" />
+      </Switch>
+    </>
   );
 }
-
-export default App;
