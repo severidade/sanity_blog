eef2c0c142ed60ca2f45047e8773af98d1169940
severo <oliveira.severo@gmail.com>
2023-04-28 23:13:50 -0300

    feat: :rocket: Cria no Navbar

--- src/componentes/Navbar.js
+++ src/componentes/Navbar.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 export default function Navbar() {
   return(
-    <>
-      <p>Navbar é aqui</p>
-    </>
+    <header>
+      <div>
+        <nav>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/post">Blog Posts</NavLink>
+        </nav>
+      </div>
+    </header>
   )
 }
