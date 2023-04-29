67c8936d3a9290850c7dcd30debaa57ce9a7c627
severo <oliveira.severo@gmail.com>
2023-04-28 22:31:26 -0300

    feat: :rocket: Cria as páginas do blog

---
+++ src/pages/BlogPosts.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function BlogPosts() {
+
+  return(
+    <div>
+      <h1>Todos os posts</h1>
+    </div>
+  )
+}
---
+++ src/pages/SinglePost.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function SinglePost() {
+
+  return(
+    <div>
+      <h1>Single page</h1>
+    </div>
+  )
+}
